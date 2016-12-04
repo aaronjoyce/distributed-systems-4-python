3ddfb1af9c0d2d69b89990c32c649761c82e0ed4
Aaron Joyce <joyceaa@tcd.ie>
2016-12-04 15:28:55 +0100

    Adding to setup script

--- setup.py
+++ setup.py
@@ -17,10 +17,11 @@ db = connection.project # equal to > use test_database
 servers = db.servers
 
 db.servers.drop()
+db.directories.drop()
+db.files.drop()
 
 m = hashlib.md5()
 m.update("127.0.0.1" + ":" + "8092")
 db.servers.insert({"reference": m.hexdigest(), "host": "127.0.0.1", "port": "8092", "is_master": True, "in_use": False})
 m.update("127.0.0.1" + ":" + "8093")
 db.servers.insert({"reference": m.hexdigest(), "host": "127.0.0.1", "port": "8093", "is_master": False, "in_use": False})
-
